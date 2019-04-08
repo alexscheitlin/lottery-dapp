e68433b823a7a011e23fbfcdb011463893e1e01e
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-04-08 21:00:25 +0200

    change display order of current/end block when game ended

--- client/src/Components/Dashboard/Drawing.js
+++ client/src/Components/Dashboard/Drawing.js
@@ -3,22 +3,41 @@ import React from 'react'
 import { Statistic, Icon } from 'semantic-ui-react';
 
 const drawing = (props) => {
-  return (
-    <Statistic.Group horizontal size='mini'>
-      <Statistic>
-        <Statistic.Value><Icon name='square outline' /> # {props.startBlock || 0}</Statistic.Value>
-        <Statistic.Label>start block</Statistic.Label>
-      </Statistic>
-      <Statistic>
-        <Statistic.Value><Icon name='square outline' /> # {props.currentBlock || 0}</Statistic.Value>
-        <Statistic.Label>current block</Statistic.Label>
-      </Statistic>
-      <Statistic>
-        <Statistic.Value><Icon name='square outline' /> # {props.endBlock || 0}</Statistic.Value>
-        <Statistic.Label>next drawing</Statistic.Label>
-      </Statistic>
-    </Statistic.Group>
-  )
+  if (props.currentBlock >= props.endBlock) {
+    return (
+      <Statistic.Group horizontal size='mini'>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.startBlock || 0}</Statistic.Value>
+          <Statistic.Label>start block</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.endBlock || 0}</Statistic.Value>
+          <Statistic.Label>end block</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.currentBlock || 0}</Statistic.Value>
+          <Statistic.Label>current block</Statistic.Label>
+        </Statistic>
+      </Statistic.Group>
+    )
+  } else {
+    return (
+      <Statistic.Group horizontal size='mini'>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.startBlock || 0}</Statistic.Value>
+          <Statistic.Label>start block</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.currentBlock || 0}</Statistic.Value>
+          <Statistic.Label>current block</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value><Icon name='square outline' /> # {props.endBlock || 0}</Statistic.Value>
+          <Statistic.Label>next drawing</Statistic.Label>
+        </Statistic>
+      </Statistic.Group>
+    )
+  }
 }
 
 export default drawing;
