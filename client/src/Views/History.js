b6ff9438a52aa07ec55921f6ed961e01ad999139
nikzaugg <zauggnik@gmail.com>
2019-04-16 10:35:01 +0200

    #5 test web3 connection with getJackpot() and displaying it in the History View

--- client/src/Views/History.js
+++ client/src/Views/History.js
@@ -1,21 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Wrapper from "../Components/shared/Wrapper";
+import LotteryContract from "../contracts/Lottery.json";
+import getWeb3 from "../utils/getWeb3";
+import {weiToEther, etherToWei} from "../utils/conversion";
 
-import {Grid} from 'semantic-ui-react';
+import { Grid, Segment } from "semantic-ui-react";
 
-const history = () => {
-  return (
-    <Wrapper>
-      <Grid>
-        <Grid.Row>
-        <Grid.Column>
-          History Component
-        </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Wrapper>
-  );
-};
+class History extends Component {
+  _isMounted = false;
 
-export default history;
+  state = {
+    web3: null,
+    contract: null,
+    games: null,
+    jackpot: null
+  };
+
+  loadWeb3 = async () => {
+    try {
+      // get network provider and web3 instance
+      const web3 = await getWeb3();
+
+      // get the contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = LotteryContract.networks[networkId];
+      const contract = new web3.eth.Contract(
+        LotteryContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      // set web3 and contract to the state
+      if (this._isMounted) {
+        this.setState({
+          web3,
+          contract
+        });
+      }
+
+      this.fetchData();
+    } catch (error) {
+      // catch any errors for any of the above operations
+      console.error(error);
+      alert(
+        "Failed to load web3, accounts, or contract. Check console for details."
+      );
+    }
+
+    this.fetchDataPolling();
+  }
+
+  fetchDataPolling = () => {
+    // periodically check for changes of the data in the blockhain
+    setInterval(() => {
+      this.fetchData();
+    }, 2000);
+  };
+
+  fetchData = async () => {
+    const { contract, web3 } = this.state;
+    this.updateJackpot(contract);
+  }
+
+
+  updateJackpot = async contract => {
+    const jackpot = await contract.methods.getJackpot().call();
+    if (this._isMounted) {
+      this.setState({ jackpot: weiToEther(jackpot) });
+    }
+  };
+
+  componentDidMount = () => {
+    this._isMounted = true;
+    this.loadWeb3();
+  }
+
+  componentWillUnmount = () => {
+    this._isMounted = false;
+    clearInterval(this.fetchDataPolling);
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment>There are {this.state.jackpot || 0} ETH in the Jackpot right now</Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Wrapper>
+    );
+  }
+}
+
+export default History;
