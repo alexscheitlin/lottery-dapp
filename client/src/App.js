64b6045392fa916c58c038409294f1009abdcb29
nikzaugg <zauggnik@gmail.com>
2019-04-16 09:58:07 +0200

    styled menu added

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
+// Router
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 // Views
 import Home from "./Views/Home";
 import History from "./Views/History";
+
 // Components
 import SiteHeader from "./Components/shared/SiteHeader";
 
-const app = () => {
-  return (
-    <div>
-      <SiteHeader />
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/history">History</Link>
-            </li>
-          </ul>
+import { Menu, Segment, Container } from "semantic-ui-react";
 
-          <hr />
+class App extends Component {
+  state = { activeItem: "home" };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div>
+        <Router>
+          <SiteHeader />
+
+          <Container text>
+            <Menu pointing secondary>
+              <Menu.Item
+                name="home"
+                active={activeItem === "home"}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/"
+              />
+              <Menu.Item
+                name="history"
+                active={activeItem === "history"}
+                onClick={this.handleItemClick}
+                as={Link}
+                to="/history"
+              />
+            </Menu>
+          </Container>
 
           <Route exact path="/" component={Home} />
           <Route path="/history" component={History} />
-        </div>
-      </Router>
-    </div>
-  );
-};
+        </Router>
+      </div>
+    );
+  }
+}
 
-export default app;
+export default App;
--- client/src/Views/History.js
+++ client/src/Views/History.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import Wrapper from "../Components/shared/Wrapper";
+
+import {Grid} from 'semantic-ui-react';
 
 const history = () => {
   return (
-    <div>History</div>
-  )
-}
+    <Wrapper>
+      <Grid>
+        <Grid.Row>
+        <Grid.Column>
+          History Component
+        </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default history;
