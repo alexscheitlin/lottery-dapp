7aeae5901cb4ece8e3c3d5928a5fd5ac3ec3cd02
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-05-02 14:29:38 +0200

    reorganize Lottery SC (no code removal, #31)

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,55 +3,81 @@ pragma solidity ^0.5.0;
 import "./RandomNumberOracle.sol";
 
 contract Lottery {
-    
-    uint256 public constant GAME_LENGTH = 3;          // number of blocks
-    uint256 public constant TICKET_PRICE = 1 ether;
-    uint256 public constant REFUND_AMOUNT = 0.1 ether;
-    uint256 public constant MIN_NUMBER = 1;             // minimum required number in a ticket (inclusive)
-    uint256 public constant MAX_NUMBER = 5;             // maximum allowed number in a ticket (inclusive)
-    uint256 public constant MAX_AMOUNT_TICKETS = 3;
-    uint256 public constant MAX_PARTICIPANTS = 50;
-    uint256 public constant NUMBERS_PER_TICKET = 2;
-    
+
+    // /////////////////////////////////////////////////////////////////
+    // structs
+    // /////////////////////////////////////////////////////////////////
+
     struct Game {
         uint256 startBlock;
         uint256 endBlock;
         uint256 drawBlock;
+
         uint256[] luckyNumbers;
+
         mapping (uint256 => Participant) participants;
         mapping (uint256 => address payable) winners;
-        
+
         // keep track of number of participants and winners to iterate
         uint256 numberOfParticipants;
         uint256 numberOfWinners;
-        
+
         // keep track of jackpot
         uint256 jackpot;
 
         // keep track of lottery resolver
         address resolver;
     }
-    
+
     struct Participant {
         address payable addr;
         uint256[][] tickets;
     }
-    
-    Game public currentGame;
-    
+
+
+    // /////////////////////////////////////////////////////////////////
+    // constants
+    // /////////////////////////////////////////////////////////////////
+
+    uint256 public constant GAME_LENGTH = 3;            // number of blocks
+    uint256 public constant MAX_AMOUNT_TICKETS = 3;
+    uint256 public constant MAX_PARTICIPANTS = 50;
+    uint256 public constant MAX_NUMBER = 5;             // maximum allowed number in a ticket (inclusive)
+    uint256 public constant MIN_NUMBER = 1;             // minimum required number in a ticket (inclusive)
+    uint256 public constant NUMBERS_PER_TICKET = 2;
+    uint256 public constant REFUND_AMOUNT = 0.1 ether;
+    uint256 public constant TICKET_PRICE = 1 ether;
+
+
+    // /////////////////////////////////////////////////////////////////
+    // variables
+    // /////////////////////////////////////////////////////////////////
+
     RandomNumberOracle oracle;
-    
+
+    Game public currentGame;
+
     mapping (uint256 => Game) public finishedGames;
     uint256 numberOfGames = 0;
+
+
+    // /////////////////////////////////////////////////////////////////
+    // constructor
+    // /////////////////////////////////////////////////////////////////
 
     constructor() public {
         currentGame = createNewGame(GAME_LENGTH);
         oracle = new RandomNumberOracle();
     }
-    
+
+
     // /////////////////////////////////////////////////////////////////
-    // pubic functions
+    // public functions
     // /////////////////////////////////////////////////////////////////
+
+    // /////////////////////////////////
+    // core functions
+    // /////////////////////////////////
     
     function buyTicket(uint256[] memory numbers) public payable {
         // verify that enough ether was sent
@@ -68,9 +94,6 @@ contract Lottery {
             require(numbers[i] >= MIN_NUMBER);
             require(numbers[i] <= MAX_NUMBER);
         }
-        
-        // verify that every number is only represented at max once
-        // TODO
         
         // record the sender's address and the received numbers
         bool foundParticipant = false;
@@ -182,6 +205,10 @@ contract Lottery {
         // start new game
         currentGame = createNewGame(GAME_LENGTH);
     }
+
+    // /////////////////////////////////
+    // getter functions (current game)
+    // /////////////////////////////////
     
     // returns the number of tickets in the current game associated with the sender's address
     function getMyTicketCountOfCurrentGame() public view returns(uint256) {
@@ -217,75 +244,53 @@ contract Lottery {
             return 0;
         }
     }
-    
-    // returns whether the current game has started
-    function hasGameStarted() public view returns(bool) {
-        return block.number >= currentGame.startBlock;
-    }
-    
-    // returns whether the current game has ended
-    function hasGameEnded() public view returns(bool) {
-        return block.number > currentGame.endBlock;
-    }
-    
-    // returns whether the current game is ongoing
-    function isGameOngoing() public view returns(bool) {
-        return this.hasGameStarted() && !this.hasGameEnded();
-    }
-    
-    // returns whether the game is ready to draw numbers
-    function isNumberDrawable() public view returns(bool) {
-        return block.number > currentGame.drawBlock;
-    }
-    
-    function getNumberOfFinishedGames() public view returns(uint256) {
-        return numberOfGames;
-    }
-    
+
+    // TODO: remove because currentGame is public
     function getStartBlockOfCurrentGame() public view returns(uint256) {
         return currentGame.startBlock;
     }
-    
+
+    // TODO: remove because currentGame is public
     function getEndBlockOfCurrentGame() public view returns(uint256) {
         return currentGame.endBlock;
     }
-    
+
+    // TODO: remove because currentGame is public
     function getDrawBlockOfCurrentGame() public view returns(uint256){
         return currentGame.drawBlock;
     }
-    
-    function getCurrentBlock() public view returns(uint256) {
-        return block.number;
+
+    // /////////////////////////////////
+    // getter functions (finished games)
+    // /////////////////////////////////
+
+    function getNumberOfFinishedGames() public view returns(uint256) {
+        return numberOfGames;
     }
 
-    // get luck numbers of a specific (finished) game
-    function getLuckyNumbers(uint256 _gameIndex) public view returns(uint256[] memory){
-        return finishedGames[_gameIndex].luckyNumbers;
-    }
-
-    // get all participants in a specific game
+    // get all participants in a specific (finished) game
     function getParticipants(uint256 gameIndex) public view returns(address[] memory _participantsAddr){
         _participantsAddr = new address[](finishedGames[gameIndex].numberOfParticipants);
-        
+
         for(uint256 i = 0; i < finishedGames[gameIndex].numberOfParticipants; i++){
             _participantsAddr[i] = finishedGames[gameIndex].participants[i].addr;
         }
-        
+
         return _participantsAddr;
     }
-    
-    // get the winners of a specific game
+
+    // get the winners of a specific (finished) game
     function getWinners(uint256 gameIndex) public view returns(address[] memory _winners){
         _winners = new address[](finishedGames[gameIndex].numberOfWinners);
-        
+
         for(uint256 i = 0; i < finishedGames[gameIndex].numberOfWinners; i++){
             _winners[i] = finishedGames[gameIndex].winners[i];
         }
-        
+
         return _winners;
     }
-    
-    // get the number of tickets of an address in a specific game
+
+    // get the number of tickets of an address in a specific (finished) game
     function getTicketCount(uint256 _gameIndex, address _address) public view returns(uint256) {
         for(uint256 i = 0; i < finishedGames[_gameIndex].numberOfParticipants; i++){
             if(finishedGames[_gameIndex].participants[i].addr == _address){
@@ -294,7 +299,7 @@ contract Lottery {
         }
     }
 
-    // get the numbers of a ticket of an address in a specific game
+    // get the numbers of a ticket of an address in a specific (finished) game
     function getTicketNumbers(uint256 _gameIndex, uint256 _ticketIndex, address _address) public view returns(uint256[] memory){
         for(uint256 i = 0; i < finishedGames[_gameIndex].numberOfParticipants; i++){
             if(finishedGames[_gameIndex].participants[i].addr == _address){
@@ -302,15 +307,49 @@ contract Lottery {
             }
         }
     }
-    
+
+    // get luck numbers of a specific (finished) game
+    function getLuckyNumbers(uint256 _gameIndex) public view returns(uint256[] memory){
+        return finishedGames[_gameIndex].luckyNumbers;
+    }
+
+    // /////////////////////////////////
+    // misc
+    // /////////////////////////////////
+
+    function getCurrentBlock() public view returns(uint256) {
+        return block.number;
+    }
+
+    // returns whether the current game has started
+    function hasGameStarted() public view returns(bool) {
+        return block.number >= currentGame.startBlock;
+    }
+
+    // returns whether the current game has ended
+    function hasGameEnded() public view returns(bool) {
+        return block.number > currentGame.endBlock;
+    }
+
+    // returns whether the current game is ongoing
+    function isGameOngoing() public view returns(bool) {
+        return this.hasGameStarted() && !this.hasGameEnded();
+    }
+
+    // returns whether the game is ready to draw numbers
+    function isNumberDrawable() public view returns(bool) {
+        return block.number > currentGame.drawBlock;
+    }
+
     // This function is only used to force ganache to add a block and thus controll the speed of the blockchain manually
     // TODO: remove as soon as it is not needed anymore
     function skipBlock() public pure {}
-    
+
+
     // /////////////////////////////////////////////////////////////////
     // private functions
     // /////////////////////////////////////////////////////////////////
-    
+
     function createNewGame(uint256 gameLength) private view returns(Game memory) {
         uint256[] memory luckyNumbers;
         Game memory newGame = Game({
@@ -323,7 +362,7 @@ contract Lottery {
             jackpot: 0,
             resolver: address(0)
         });
-        
+
         return newGame;
     }
 }
